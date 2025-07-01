59eb99b306d87c9598ad11a2338d874dd1ce6a10
santino1919 <102044326+santinoo1919@users.noreply.github.com>
2025-07-01 10:14:25 +0100

    fix: removed cluster lib

--- src/app/MapClientWrapper.tsx
+++ src/app/MapClientWrapper.tsx
@@ -1,6 +1,5 @@
 "use client";
 import dynamic from "next/dynamic";
-// @ts-expect-error: Dynamic import of client component, type not needed
 const MapView = dynamic(() => import("./MapView"), { ssr: false });
 
 export default function MapClientWrapper() {
