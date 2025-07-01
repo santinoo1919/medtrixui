984dab960301441c410212108a065279364fdedf
santino1919 <102044326+santinoo1919@users.noreply.github.com>
2025-07-01 10:46:12 +0100

    fix: minimal mapview to test

--- src/app/MapView.tsx
+++ src/app/MapView.tsx
@@ -1,22 +1,18 @@
 "use client";
 
 import { MapContainer, TileLayer } from "react-leaflet";
-import WrecksGeoJSON from "./WrecksGeoJSON";
 
 export default function MapView() {
   return (
-    <div className="relative w-full h-screen">
-      <MapContainer
-        center={[43.3, 5.4]}
-        zoom={10}
-        style={{ height: "100vh", width: "100%" }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <WrecksGeoJSON />
-      </MapContainer>
-    </div>
+    <MapContainer
+      center={[43.3, 5.4]}
+      zoom={10}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+    </MapContainer>
   );
 }
