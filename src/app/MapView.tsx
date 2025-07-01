4fe78c71ff8ef121377900ca9cda85b9c9ff2810
santino1919 <102044326+santinoo1919@users.noreply.github.com>
2025-07-01 10:48:59 +0100

    fix: typing workaround

--- src/app/MapView.tsx
+++ src/app/MapView.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, type MapContainerProps } from "react-leaflet";
 
 export default function MapView() {
+  const props: MapContainerProps = {
+    center: [43.3, 5.4],
+    zoom: 10,
+    style: { height: "100vh", width: "100%" },
+  };
   return (
-    <MapContainer
-      center={[43.3, 5.4]}
-      zoom={10}
-      style={{ height: "100vh", width: "100%" }}
-    >
+    <MapContainer {...props}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; OpenStreetMap contributors"
